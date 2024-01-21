205a8873940d057c937fd1903481372a0e482907
Haluk Tütüncü <112336855+haluktutuncu@users.noreply.github.com>
2024-01-21 14:10:21 +0300

    Version 1 (default)

---
+++ src/particle.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "ofMain.h"
+
+class particle
+{
+public:
+    particle();
+    
+    void setup();
+    void update(float speed,float noise);
+    void draw(float sizeDot);
+    
+    ofVec2f pos;
+    ofVec2f vel;
+    ofVec2f frc;
+    
+    float drag;
+    
+    ofVec2f uniqueVal;
+    float size;
+
+    
+    
+};
